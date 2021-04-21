f7ba32710b24b606ec70e51357e63c36e5dc15d7
Mohamed Sahel <36473352+mohamedsahel@users.noreply.github.com>
2021-04-21 14:48:21 +0000

    Create content.js

---
+++ content.js
@@ -0,0 +1,76 @@
+{
+  profile: {
+    name: 'Mohamed Sahel',
+    job: 'React/Node Developer',
+    imageUrl: 'https://i.imgur.com/q5N8V8g.jpg',
+    social: {
+      github: 'https://github.com/mohamedsahel',
+      linkedin: 'https://linkedin.com/in/mohamedsahel',
+      email: 'mailto:hi@mohamedsahel.com'
+    },
+    resume: 'https://githubcontent.com'
+  },
+  about: {
+    title: 'about me',
+    description:
+      'Creative Front End Developer with over 2 years of experience providing high-impact web solutions for diverse industry organizations. Skilled in designing, developing and testing multiple web-based applications incorporating a range of technologies.'
+  },
+  projects: {
+    title: 'projects',
+    subtitle: 'Creative Front End Developer with over 2 years of experience providing high-impact.',
+    list: [
+      {
+        name: 'Project Name',
+        description:
+          'Creative Front End Developer with over 2 years of experience providing high-impact web solutions for diverse industry organizations',
+        imageUrl: 'https://i.imgur.com/6cOScEH.png?1',
+        githubUrl: 'https://github.com/mohamedsahel/qat',
+        projectUrl: 'https://qat-cli.vercel.com'
+      },
+      {
+        name: 'Project Name',
+        description:
+          'Creative Front End Developer with over 2 years of experience providing high-impact web solutions for diverse industry organizations',
+        imageUrl: 'https://i.imgur.com/6cOScEH.png?1',
+        githubUrl: 'https://github.com/mohamedsahel/qat',
+        projectUrl: 'https://qat-cli.vercel.com'
+      },
+      {
+        name: 'Project Name',
+        description:
+          'Creative Front End Developer with over 2 years of experience providing high-impact web solutions for diverse industry organizations',
+        imageUrl: 'https://i.imgur.com/6cOScEH.png?1',
+        githubUrl: 'https://github.com/mohamedsahel/qat',
+        projectUrl: 'https://qat-cli.vercel.com'
+      }
+    ]
+  },
+  testimonials: {
+    title: 'testimonials',
+    list: [
+      {
+        author: 'Paul',
+        organisation: 'bluebox',
+        testimonial: 'Awesome working with Mohamed - he is a fast worker, and a good communicator',
+        rating: 5
+      },
+      {
+        author: 'Taco',
+        organisation: 'Stainless media',
+        testimonial:
+          'Excellent work, excellent communication, quick response to questions and suggestions and I would definitely recommend Mohamed to anyone!',
+        rating: 5
+      },
+      {
+        author: 'Martyn',
+        organisation: '',
+        testimonial: 'Excellent, quick and professional job. Would highly recommend +++++',
+        rating: 5
+      }
+    ]
+  },
+  contact: {
+    title: 'contact',
+    subtitle: 'this is just an subtiltefor the contact section'
+  }
+}
