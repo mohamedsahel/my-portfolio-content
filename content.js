62741197ea61336e67edd99a9a23cdaaa238d1cd
Mohamed Sahel <36473352+mohamedsahel@users.noreply.github.com>
2021-04-29 14:06:59 +0000

    Update content.js

--- content.js
+++ content.js
@@ -1,117 +1,135 @@
 {
-  profile: {
-    name: 'Mohamed Sahel',
-    job: 'Full-Stack Web Developer',
-    imageUrl: 'https://i.imgur.com/q5N8V8g.jpg',
-    social: {
-      github: 'https://github.com/mohamedsahel',
-      linkedin: 'https://linkedin.com/in/mohamedsahel',
-      email: 'mailto:hi@mohamedsahel.com'
-    },
-    resume: 'https://githubcontent.com'
-  },
-  about: {
-    title: 'about me',
-    description:
-      'Creative Full-Stack web developer, adept in all stages of advanced web development, Highly Skilled in user interface, developing, and debugging processes, designed and built multiple web-based applications and internal tools, incorporating a range of technologies and programming languages.'
-    skills: [
-      {
-        groupName: 'Languages & Frameworks',
-        list: ['HTML', 'Javascript', 'Typescript', 'Next.js', 'Gatsby.js', 'Node.js']
+   "profile":{
+      "name": "Mohamed Sahel",
+      "job": "Full-Stack Web Developer",
+      "imageUrl": "https://i.imgur.com/q5N8V8g.jpg",
+      "social": {
+         "github": "https://github.com/mohamedsahel",
+         "linkedin": "https://linkedin.com/in/mohamedsahel",
+         "email": "mailto:hi@mohamedsahel.com"
       },
-      {
-        groupName: 'Top Libraries',
-        list: ['React', 'Tailwind', 'Chakra UI', 'Express', 'Apollo', 'Mongoose']
-      },
-      {
-        groupName: 'Third Party Tools',
-        list: ['Firebase', 'Airtable', 'Mailjet', 'Stripe', 'Paypal']
-      },
-      {
-        groupName: 'Other Skills',
-        list: ['CSS', 'Graphql', 'Prisma', 'Git', 'Adobe XD', 'Figma']
-      },
-    ]
-  },
-  projects: {
-    title: 'projects',
-    subtitle: 'This is a hand-picked list of projects that I have personally designed, developed and deployed to production',
-    list: [
-      {
-        name: 'SalesCovers',
-        description:
-          'Business service website for a team of book cover designers, with the integration of 3rd party APIs for payments processing, emails and orders storage',
-        imageUrl: 'https://i.imgur.com/HJmf2Fu.jpg?1',
-        projectUrl: 'https://salescovers.com'
-      },
-      {
-        name: 'Earfun',
-        description:
-          'Full features headphones single page online store application with implementation of cart & checkout process + dark mode support',
-        imageUrl: 'https://i.imgur.com/faHafHi.jpg?1',
-        githubUrl: 'https://github.com/mohamedsahel/earfun',
-        projectUrl: 'https://earfun.herokuapp.com'
-      },
-      {
-        name: 'Weathi',
-        description:
-          'Beautiful look single page weather application, using Google maps and OpenWeather APIs',
-        imageUrl: 'https://i.imgur.com/A0zVl4U.png?1',
-        githubUrl: 'https://github.com/mohamedsahel/weathi',
-        projectUrl: 'https://weathi.herokuapp.com'
-      },
-      {
-        name: 'Portfio',
-        description:
-          'Well Designed open source personal portfolio website with interactive animations and dark mode support',
-        imageUrl: 'https://i.imgur.com/7eya5ss.jpg?1',
-        githubUrl: 'https://github.com/mohamedsahel/portfio',
-        projectUrl: 'https://portfio.vercel.app'
-      },
-       {
-        name: 'Roomz',
-        description:
-          'Modern, real-time and well-structured chat room back-end API with Implementation of advanced functionalities: authentication - authorization - database management and real-time connections',
-        imageUrl: 'https://i.imgur.com/Xcjjrkv.jpg?1',
-        githubUrl: 'https://github.com/mohamedsahel/roomz'
-      },
-      {
-        name: 'Qat',
-        description:
-          'Open source CLI tool that helps developers automate workflow tasks using custom CLI commands without reinventing another CLI tool, “Qat stands for Quickly Automate Tasks”',
-        imageUrl: 'https://i.imgur.com/ubmCYxz.jpg?1',
-        githubUrl: 'https://github.com/mohamedsahel/qat',
-        projectUrl: 'https://qat-cli.vercel.app'
-      }
-    ]
-  },
-  testimonials: {
-    title: 'testimonials',
-    duration: 15000, 
-    list: [
-      {
-        author: 'Paul',
-        organisation: 'bluebox',
-        testimonial: 'Awesome working with Mohamed - he is a fast worker, and a good communicator',
-        rating: 5
-      },
-      {
-        author: 'Taco',
-        organisation: 'Stainless media',
-        testimonial:
-          'Excellent work, excellent communication, quick response to questions and suggestions and I would definitely recommend Mohamed to anyone!',
-        rating: 5
-      },
-      {
-        author: 'Martyn',
-        organisation: '',
-        testimonial: 'Excellent, quick and professional job. Would highly recommend +++++',
-        rating: 5
-      }
-    ]
-  },
-  contact: {
-    title: 'contact',
-    subtitle: 'Have a question, or project idea, Please feel free to let your message, I reply in less than 24h 😉'
-  }
+      "resume": "https://githubcontent.com"
+   },
+   "about": {
+      "title": "about me",
+      "description": "Creative Full-Stack web developer, adept in all stages of advanced web development, Highly Skilled in user interface, developing, and debugging processes, designed and built multiple web-based applications and internal tools, incorporating a range of technologies and programming languages.""skills":[
+         {
+            "groupName": "Languages & Frameworks",
+            "list": [
+               "HTML",
+               "Javascript",
+               "Typescript",
+               "Next.js",
+               "Gatsby.js",
+               "Node.js"
+            ]
+         },
+         {
+            "groupName": "Top Libraries",
+            "list": [
+               "React",
+               "Tailwind",
+               "Chakra UI",
+               "Express",
+               "Apollo",
+               "Mongoose"
+            ]
+         },
+         {
+            "groupName": "Third Party Tools",
+            "list": [
+               "Firebase",
+               "Airtable",
+               "Mailjet",
+               "Stripe",
+               "Paypal"
+            ]
+         },
+         {
+            "groupName": "Other Skills",
+            "list":[
+               "CSS",
+               "Graphql",
+               "Prisma",
+               "Git",
+               "Adobe XD",
+               "Figma"
+            ]
+         }
+      ]
+   },
+   "projects": {
+      "title": "projects",
+      "subtitle": "This is a hand-picked list of projects that I have personally designed, developed and deployed to production",
+      "list":[
+         {
+            "name": "SalesCovers",
+            "description": "Business service website for a team of book cover designers, with the integration of 3rd party APIs for payments processing, emails and orders storage",
+            "imageUrl": "https://i.imgur.com/HJmf2Fu.jpg?1",
+            "projectUrl": "https://salescovers.com"
+         },
+         {
+            "name": "Earfun",
+            "description": "Full features headphones single page online store application with implementation of cart & checkout process + dark mode support",
+            "imageUrl": "https://i.imgur.com/faHafHi.jpg?1",
+            "githubUrl": "https://github.com/mohamedsahel/earfun",
+            "projectUrl": "https://earfun.herokuapp.com"
+         },
+         {
+            "name": "Weathi",
+            "description": "Beautiful look single page weather application, using Google maps and OpenWeather APIs",
+            "imageUrl": "https://i.imgur.com/A0zVl4U.png?1",
+            "githubUrl": "https://github.com/mohamedsahel/weathi",
+            "projectUrl": "https://weathi.herokuapp.com"
+         },
+         {
+            "name": "Portfio",
+            "description": "Well Designed open source personal portfolio website with interactive animations and dark mode support",
+            "imageUrl": "https://i.imgur.com/7eya5ss.jpg?1",
+            "githubUrl": "https://github.com/mohamedsahel/portfio",
+            "projectUrl": "https://portfio.vercel.app"
+         },
+         {
+            "name": "Roomz",
+            "description": "Modern, real-time and well-structured chat room back-end API with Implementation of advanced functionalities: authentication - authorization - database management and real-time connections",
+            "imageUrl": "https://i.imgur.com/Xcjjrkv.jpg?1",
+            "githubUrl": "https://github.com/mohamedsahel/roomz"
+         },
+         {
+            "name": "Qat",
+            "description": "Open source CLI tool that helps developers automate workflow tasks using custom CLI commands without reinventing another CLI tool, “Qat stands for Quickly Automate Tasks”",
+            "imageUrl": "https://i.imgur.com/ubmCYxz.jpg?1",
+            "githubUrl": "https://github.com/mohamedsahel/qat",
+            "projectUrl": "https://qat-cli.vercel.app"
+         }
+      ]
+   },
+   "testimonials": {
+      "title":" testimonials",
+      "duration": 15000,
+      "list":[
+         {
+            "author": "Paul",
+            "organisation": "bluebox",
+            "testimonial": "Awesome working with Mohamed - he is a fast worker, and a good communicator",
+            "rating": 5
+         },
+         {
+            "author": "Taco",
+            "organisation": "Stainless media",
+            "testimonial": "Excellent work, excellent communication, quick response to questions and suggestions and I would definitely recommend Mohamed to anyone!",
+            "rating": 5
+         },
+         {
+            "author": "Martyn",
+            "organisation": "",
+            "testimonial": "Excellent, quick and professional job. Would highly recommend +++++",
+            "rating": 5
+         }
+      ]
+   },
+   "contact":{
+      "title": "contact",
+      "subtitle": "Have a question, or project idea, Please feel free to let your message, I reply in less than 24h 😉"
+   }
 }
